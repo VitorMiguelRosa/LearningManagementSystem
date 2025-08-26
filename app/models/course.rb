0280e69969273315195a1203eff52a31aa2fc5eb
VitorMiguelRosa <vitorvlogg3rmito@gmail.com>
2025-08-26 18:19:35 -0300

    added categories,navbar and some  styling

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ApplicationRecord
   has_one_attached :image do |attachable|
-    attachable.variant :thumb, resize_to_limit: [ 100, 100 ]
+    attachable.variant :thumb, resize_to_limit: [ 1080, 720 ]
   end
   has_many :lessons
+  has_and_belongs_to_many :categories
 end
